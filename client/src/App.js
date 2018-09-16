6224e01c0be5409a27d746da9def686a1fbb4eae
Heraldo FIlho <heraldo663@gmail.com>
2018-09-16 17:01:56 -0300

    front end setup finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>FileServer</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar></Navbar>
+          <Footer></Footer>
+        </div>
+      </Router>
     );
   }
 }
