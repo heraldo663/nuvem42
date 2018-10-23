2b7ec8bb06bcd212712da5884b021c81a87fea17
Heraldo FIlho <heraldo663@gmail.com>
2018-10-23 15:27:44 -0300

    basic auth tests added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,21 +1,26 @@
-const { User } = require('../models');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   async register(req, res) {
     try {
-      const user = await User.findOne({ where: { email: req.body.email } })
+      if (req.body.password.length <= 4) {
+        res.status(400).send({
+          success: false,
+          error: "password need to have more then 4 characaters"
+        });
+      }
+      const user = await User.findOne({ where: { email: req.body.email } });
       if (user) {
-        let errors = { email: 'Email already exists' };
+        let errors = { email: "Email already exists" };
         return res.status(400).json({ success: false, errors });
       } else {
-
         const newUser = {
           username: req.body.username,
           email: req.body.email,
           password: req.body.password
-        }
+        };
 
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -27,22 +32,19 @@ module.exports = {
           });
         });
       }
-
     } catch (error) {
-      res.status(500).json({ error, success: false })
+      res.status(500).json({ error, success: false });
     }
-
-
-
   },
 
   async login(req, res) {
     try {
       // Find user by email
-      const { email, password } = req.body
-      const user = await User.findOne({ where: { email } })
+      const { email, password } = req.body;
 
-      const isMatch = await bcrypt.compare(password, user.password)
+      const user = await User.findOne({ where: { email } });
+
+      const isMatch = await bcrypt.compare(password, user.password);
       if (isMatch) {
         // User Matched
         const payload = { id: user.id, username: user.username }; // Create JWT Payload
@@ -54,17 +56,16 @@ module.exports = {
           (err, token) => {
             return res.json({
               success: true,
-              token: 'Bearer ' + token
+              token: "Bearer " + token
             });
           }
         );
       } else {
-        let errors = { password: 'Password incorrect' }
+        let errors = { password: "Password incorrect" };
         return res.status(400).json({ errors, success: false });
       }
     } catch (error) {
-      res.status(500).json({ error, success: false })
+      res.status(400).json({ error: "email not found", success: false });
     }
-
-  },
-}
+  }
+};
---
+++ tests/integration/auth.test.js
@@ -0,0 +1,77 @@
+const supertest = require("supertest");
+const faker = require("faker");
+const app = require("../../app");
+let server, request;
+
+beforeAll(done => {
+  server = app.listen(done);
+  request = supertest.agent(server);
+});
+afterAll(async done => {
+  server.close(done);
+});
+
+describe("register", () => {
+  it("should register a user", async () => {
+    const newUser = {
+      username: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+
+    const res = await request.post("/api/auth/register").send(newUser);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("name", newUser.name);
+  });
+  it("should fail to register a user with password < 4 chars", async () => {
+    const newUser = {
+      username: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: "123"
+    };
+
+    const res = await request.post("/api/auth/register").send(newUser);
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty("success", false);
+  });
+  it("should fail to register a user that already singup", async () => {
+    const newUser = {
+      username: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+
+    await request.post("/api/auth/register").send(newUser);
+    const res = await request.post("/api/auth/register").send(newUser);
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty("success", false);
+  });
+});
+
+describe("login", () => {
+  it("should fail to login a user", async () => {
+    const user = {
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+
+    const res = await request.post("/api/auth/login").send(user);
+
+    expect(res.status).toBe(400);
+  });
+  it("should login a user", async () => {
+    const user = {
+      email: "test@test.com",
+      password: "123456",
+      username: "tester"
+    };
+
+    await request.post("/api/auth/register").send(user);
+    delete user.username;
+
+    const res = await request.post("/api/auth/login").send(user);
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("success", true);
+  });
+});
