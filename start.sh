497e34a9880383c02df49e2c6c682e3507c617a8
Heraldo Filho <heraldo663@gmail.com>
2018-11-08 10:04:28 -0200

    server modify

--- start.sh
+++ start.sh
@@ -1,8 +1,5 @@
 sequelize db:create
 sequelize db:migrate
-#build client
-npm run install:client
-npm run build:client
 # start the server
 pm2 start ecosystem.config.js --env production
 pm2 log fileserver
