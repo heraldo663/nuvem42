1f3bd89d81d357513bff0089d4ff610ba396669f
Heraldo FIlho <heraldo663@gmail.com>
2018-11-14 19:19:54 -0200

    fixed the typo

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { register, login } = require("../../controllers/authController");
-const passport = require('passport');
+const passport = require("passport");
 
 /* GET home page. */
 router.post(
   "/register",
   passport.authenticate("jwt", { session: false }),
- , register
+  register
 );
 router.post("/login", login);
 
